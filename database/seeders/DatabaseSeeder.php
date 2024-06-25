235ed8a4f1fcda3e1a3b681957f964183962ab8f
Juan <juanmaanuelhernandez23@gmail.com>
2024-06-24 22:52:14 -0500

    se añadieron roles y permisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user=User::factory()->create([
+             'name' => 'admin',
+             'email' => 'admin@admin.com',
+         ]);
+        $role = Role::create(['name' => 'admin']);
+        $user->assignRole('admin');
     }
 }
