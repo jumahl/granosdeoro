74d4b3b406125fb62b6080deaec65ce4bc39e1ba
Alejandro <alejandromercadobenitez@gmail.com>
2024-11-06 16:05:52 -0500

    Actualizar configuración de base de datos para entorno de pruebas y agregar seeder de productos y permisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,27 +1,48 @@
 <?php
-
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 use App\Models\User;
+use App\Models\Producto;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Crear permisos
+        $permissions = [
+            'view_any_pedidos',
+            'view_pedidos',
+            'create_pedidos',
+            'update_pedidos',
+        ];
 
-        $user=User::factory()->create([
-             'name' => 'admin',
-             'email' => 'admin@admin.com',
-         ]);
-        $role = Role::create(['name' => 'admin']);
-        $user->assignRole('admin');
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Crear usuario admin
+        $user = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+        ]);
+
+        // Crear rol admin y asignarle permisos
+        $role = Role::firstOrCreate(['name' => 'admin']);
+        $role->syncPermissions($permissions); // Asignar permisos al rol
+        $user->assignRole('admin'); // Asignar rol al usuario
+
+        // Crear productos
+        $productos = [
+            ['nombre' => 'Producto 1', 'descripcion' => 'Descripción del Producto 1', 'precio' => 100, 'cantidad_en_existencia' => 50],
+            ['nombre' => 'Producto 2', 'descripcion' => 'Descripción del Producto 2', 'precio' => 200, 'cantidad_en_existencia' => 30],
+            ['nombre' => 'Producto 3', 'descripcion' => 'Descripción del Producto 3', 'precio' => 150, 'cantidad_en_existencia' => 20],
+        ];
+
+        foreach ($productos as $producto) {
+            Producto::create($producto);
+        }
     }
 }
